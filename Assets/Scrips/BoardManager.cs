5ae61992d9fc446c3fd2a8185b7662b0b274013b
sirkhaos <alberto.torres.carreno@gmail.com>
2019-07-16 15:43:39 -0400

    creando el gameobject del tablero

---
+++ Assets/Scrips/BoardManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public static BoardManager sharedInstance;
+
+    public List<Sprite> prefabs = new List<Sprite>();
+    public GameObject currentCoin;
+    public int xSize, ySize;
+    public bool isShifting { get; set; }
+
+    private GameObject[,] coins;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (sharedInstance == null)
+        {
+            sharedInstance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void CreateInitialBoard(Vector2 offset)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

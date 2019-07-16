b1accdaffdad3c97520c2c27eae1c648073a2b3e
sirkhaos <alberto.torres.carreno@gmail.com>
2019-07-16 13:00:38 -0400

    iniciando las variables de coin

--- Assets/Scrips/Coin.cs
+++ Assets/Scrips/Coin.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    private static Color selectedColor = new Color(0.5f, 0.5f, 0.5f, 1.0f);
+    private static Coin previousSelected = null;
+
+    private SpriteRenderer spriteRenderer;
+    private bool isSelected = false;
+    private Vector2[] direcciones = new Vector2[]
+    {
+        Vector2.up,
+        Vector2.right,
+        Vector2.down,
+        Vector2.left
+    };
+
+    public int id;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
